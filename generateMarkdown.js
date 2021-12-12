976e38372c54a5afc2a82b007ecec370711aa8ea
bean <bkung188@gmail.com>
2021-12-11 16:06:25 -0800

    added to generateMarkdown

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -14,7 +14,8 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `
   # ${data.title}
-
+  [![License: MIT](https://img.shields.io/badge/License-${data.license}-yellow.svg)](https://opensource.org/licenses/MIT)
+  
   ## Description
   ${data.description}
 
@@ -23,8 +24,32 @@ function generateMarkdown(data) {
   -[Installation](#installation)
   -[Usase](#usage)
   -[Licesnse](#license)
-  -[Licesnse](#license)
-  -[Licesnse](#license)`;
+  -[Contribution](#contribution)
+  -[Test Instructions](#test)
+  -[Questions](#questions)
+  
+  ## Installation Instructions
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  
+  This application used ${data.licenses} license
+  
+  ## Contribution
+  ${data.contribution}
+  
+  ##Test Instructions
+  ${data.test}
+  
+  ## Questions
+  [Github page](http://github.com/${data.username})
+  
+  Email: ${data.email}
+  
+  ${data.additional}`;
 }
 
 module.exports = generateMarkdown;
